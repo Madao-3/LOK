38428ca01c92727a67bdc1a37e17066f14f3ccb4
Madao <chrishyman256@gmail.com>
2015-12-21 16:28:24 +0800

    Initial Commit

---
+++ LOK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LOK
+//
+//  Created by Madao on 12/21/15.
+//  Copyright © 2015 Madao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LOK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LOK
+//
+//  Created by Madao on 12/21/15.
+//  Copyright © 2015 Madao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
